f98c5d2a1b02c585e549845e3162bba4f2f0b39d
juanksaavedra <juank.saavedra@gmail.com>
2023-08-28 14:53:26 -0300

    Datos simulados distintos errores

--- Code1.R
+++ Code1.R
@@ -11,3 +11,17 @@ mod_lp <- function(longitud, n, a, b, error, semilla){
   )
   return(dt)
 }
+
+# Ejemplo datos simulados con diferentes errores ----
+dt_lp_error <- expand_grid(
+  n = 200,
+  a = 0.0001,
+  b = 2.9,
+  error = seq(0.01,1,0.1),
+  Talla = c(10, 120)) %>% 
+  group_by(n, a, b, error) %>% 
+  nest(.key = 'Tallas') %>% 
+  mutate(
+    datos_lp = purrr::map(Tallas, ~mod_lp(longitud = .$Talla, error = error, 
+                                          n = n, a = a, b = b))
+  )
