23743e911b2003e4721e4917b9b756431d377dbd
juanksaavedra <juank.saavedra@gmail.com>
2023-08-28 23:38:18 -0300

    Función de simulacion

--- Code1.R
+++ Code1.R
@@ -1,27 +1,13 @@
 # Librerias ----
-pacman::p_load(tidyverse, purrr)
+pacman::p_load(tidyverse, purrr, quarto)
 
 # Simulación modelo LP ----
 mod_lp <- function(longitud, n, a, b, error, semilla){
   dt <- tibble::tibble(
     Talla = sample(seq(longitud[1],longitud[2],1), size = n, replace = TRUE),
     Peso_medio = a*Talla^b,
-    error = rnorm(n = n, sd = error),
-    Peso = Peso_medio*exp(error)
+    sim_error = rnorm(n = n, sd = error),
+    Peso = Peso_medio*exp(sim_error)
   )
   return(dt)
 }
-
-# Ejemplo datos simulados con diferentes errores ----
-dt_lp_error <- expand_grid(
-  n = 200,
-  a = 0.0001,
-  b = 2.9,
-  error = seq(0.01,1,0.1),
-  Talla = c(10, 120)) %>% 
-  group_by(n, a, b, error) %>% 
-  nest(.key = 'Tallas') %>% 
-  mutate(
-    datos_lp = purrr::map(Tallas, ~mod_lp(longitud = .$Talla, error = error, 
-                                          n = n, a = a, b = b))
-  )
